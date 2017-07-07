7629a5c8464dee95d9175dee7572911291f51af3
matthieugomez <gomez.matthieu@gmail.com>
2017-07-07 12:41:38 -0400

    add graph

---
+++ code/4-graph.r
@@ -0,0 +1,29 @@
+library(data.table)
+library(tidyr)
+library(stringr) 
+library(ggplot2) 
+
+DT = fread("~/resultR1e7.csv")
+DT2 = fread("~/resultStata1e7.csv")
+setnames(DT, "result", "R")
+DT[, Stata := DT2[["result"]]]
+
+
+DT=gather(DT, language, value, Stata, R)
+setDT(DT)
+DT[,value:=value/60]
+DT$command=factor(DT$command, levels=rev(unique(DT$command)))
+
+
+DT[command == "open csv", value := value/3]
+DT[command == "group", value := value/2]
+DT[command == "sd large groups", value := value/7]
+DT[command == "reg 2 hfe", value := value/2]
+
+
+
+image = ggplot(DT,aes(x=command,y=value,fill=language,width=0.6)) + geom_bar(position=position_dodge(width=0.6),stat="identity")+ coord_flip()+scale_fill_discrete(breaks=c("Stata","R")) + ylab("Time (min)")
+
+ggsave(filename="~/1e7.svg", image)
+
+ggsave(filename="~/1e7.png", image)
